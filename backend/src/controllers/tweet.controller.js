581c62e9b765f17a8b2869a01dc58a4df70fd3fd
Raj <imraaj1026@gmail.com>
2026-09-19 16:08:18 +0530

    feat: implement subscription feature with controllers, models, and routes

--- backend/src/controllers/tweet.controller.js
+++ backend/src/controllers/tweet.controller.js
@@ -30,7 +30,10 @@ const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
     const { userId } = req.params;
     if (!userId) throw new ApiError(400, "user id not defined while getting user tweets");
-    const userTweets = await Tweet.findById(userId);
+    const userTweets = await Tweet.find({
+        owner:userId
+    });
+    // console.log(userTweets)
     if (!userTweets) {
         throw new ApiError(500, "user tweets do not exist or something went wrong");
     }
